a81dfa50ab0331331b46d93eba79acfa81e04c8b
RyanMG <david.ryan.hall@gmail.com>
2013-07-21 15:50:37 -0700

    Working with backbone to display projects

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 
 	// UI click to scroll to
 	$('nav').on('click', '.nav-icon', function(event) {
+	   event.preventDefault();
 	   var link = $(event.target).attr("rel");
 		$('html, body').animate({
 			scrollTop: $(link).offset().top
@@ -17,6 +18,57 @@ $(document).ready(function(){
 			scrollTop: $('#portfolio').offset().top
 		}, 1000, 'easeOutCubic');
 	});
+
+	// BACKBONE 
+	 var masterProjectList;
+
+	  $.getJSON('source/masterProjectList.json', function(data) {
+	      masterProjectList = new ProjectList(data);
+	      var masterListing = new ProjectListView();
+	  });
+
+	  var Project = Backbone.Model.extend({
+	    defaults: {
+	      title: '',
+	      client: '',
+	      scope: '',
+	      projectType: '',
+	      mainImg: "http://placehold.it/318/220"
+	    }
+	  });
+
+	  var ProjectList = Backbone.Collection.extend({
+	    model: Project,
+	  });
+
+	  var ProjectView = Backbone.View.extend({
+	    tagName: "div",
+	    className: "project-wrap",
+	    template: _.template($(this.options.templateType).html()),
+
+	    render: function() {
+	      this.$el.html(this.template(this.model.toJSON()));
+	      return this;
+	    }
+	  });
+
+	  var ProjectListView = Backbone.View.extend({
+	  	el: '#projectList',
+	    initialize: function() {
+	      this.collection = masterProjectList;
+	      this.render();
+	    },
+	    render: function() {
+	      this.$el.html("");
+	      this.collection.each(function(project) {
+		        this.renderItem(project);
+	      }, this);
+	    },
+	    renderItem: function(project) {
+	      var projectView = new ProjectView({model: project, templateType: "#theatricalProjectTemplate"});
+	      this.$el.append(projectView.render().el);
+	    }
+	  });
 
 
 });// doc ready
